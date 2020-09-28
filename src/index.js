b6b5bdd0c6371a948b40b35fbec005deb4f012ad
Artem Usatyi <badlywaves@gmail.com>
2020-09-28 13:47:51 +0300

    init: solve problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (array) {
+        if (Array.isArray(array) && array.length) {
+            return Math.min(...array);
+        }
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array) {
+        if (Array.isArray(array) && array.length) {
+            return Math.max(...array);
+        }
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let average = 0;
+    if (array) {
+        if (array.length === 1) {
+            return array[0];
+        }
+        if (Array.isArray(array) && array.length) {
+            for (let i = 0; i < array.length; i++) {
+                average += array[i];
+            }
+            return average / array.length;
+        }
+    }
+    return 0;
+};
